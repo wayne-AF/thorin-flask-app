c56f6e0a8536c5ff3b2b934c14fbec96e5a3c7f0
wayne-AF <wayne.anthony.feeney@gmail.com>
2022-12-26 19:35:44 +0000

    first commit

---
+++ run.py
@@ -0,0 +1,17 @@
+import os
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return "Hello, world"
+
+
+if __name__ == "__main__":
+    app.run(
+        host=os.environ.get("IP", "0.0.0.0"),
+        port=int(os.environ.get("PORT", "5000")),
+        # should only be true when developing. should always be false when submitting
+        debug=True)
