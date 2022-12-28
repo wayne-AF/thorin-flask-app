c8e1614a5f4e38a25aefea4db90479dd621ff8f3
wayne-AF <wayne.anthony.feeney@gmail.com>
2022-12-28 18:24:49 +0000

    add code for form

--- run.py
+++ run.py
@@ -1,8 +1,14 @@
 import os
 import json
-from flask import Flask, render_template
+# request contains form object when we post it
+# flash is used to display a message to the user
+from flask import Flask, render_template, request, flash
+if os.path.exists("env.py"):
+    import env
+
 
 app = Flask(__name__)
+app.secret_key = os.environ.get("SECRET_KEY")
 
 
 @app.route("/")
@@ -18,8 +24,23 @@ def about():
     return render_template("about.html", page_title="About", company=data)
 
 
-@app.route("/contact")
+@app.route("/about/<member_name>")
+def about_member(member_name):
+    member = {}
+    with open("data/company.json", "r") as json_data:
+        data = json.load(json_data)
+        for obj in data:
+            if obj["url"] == member_name:
+                member = obj
+    return render_template("member.html", member=member)
+
+
+@app.route("/contact", methods=["GET", "POST"])
 def contact():
+    if request.method == "POST":
+        # returns a dictionary and can use python method to retrieve info
+        flash("Thanks, {}, we have received your message.".format(
+            request.form.get("name")))
     return render_template("contact.html", page_title="Contact")
 
 
